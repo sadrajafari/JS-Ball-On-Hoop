ccff3651c00bf8d6c68bdb4d81a98a996ac3fc4b
A02366638 <a02366638@aggies.usu.edu>
2022-11-18 16:06:03 -0700

    even more refactoring/organizing

--- src/runSim.js
+++ src/runSim.js
@@ -11,22 +11,25 @@ export let nextFrameVariable = null;
 
 export function draw(equations, useEval, thetaDivId, velocityDivId, ) {
 
-
+    //Cancels the previous animation render loop of either the static or variable equation draw
     if (useEval){
         if (nextFrameVariable != null) cancelAnimationFrame(nextFrameVariable);
     } else{
         if (nextFrameStatic != null) cancelAnimationFrame(nextFrameStatic);
         }
     
+  // picks the correct canvas based on if drawing inputed eqn or not
   let canvas;
   if (useEval){
     canvas = document.querySelector('#c');
     } else {
     canvas = document.querySelector('#c2'); 
     }
+
+
+  //create camera and renderer
   const renderer = new THREE.WebGLRenderer({canvas});
   renderer.setClearColor( 0xffffff, 0);
-  //renderer.setSize(window.innerWidth, window.innerHeight);
   const viewSize = canvas.clientWidth;
   const aspectRatio = canvas.clientWidth/canvas.clientHeight;
   const near = 0.1;
@@ -35,6 +38,8 @@ export function draw(equations, useEval, thetaDivId, velocityDivId, ) {
   //const camera = new THREE.OrthographicCamera(-aspectRatio*viewSize/2,aspectRatio*viewSize/2, viewSize/2,-viewSize/2,near,far)
   camera.position.z = 500;
   const scene = new THREE.Scene();
+
+  //get vals from page
   let radius = Number(document.getElementById("radius").value);
   let tube = 3;
   let radialSegments = 16; 
@@ -46,18 +51,18 @@ export function draw(equations, useEval, thetaDivId, velocityDivId, ) {
   let angle =  Number(document.getElementById("theta").value)*Math.PI/180;
   let velocity = Number(document.getElementById("velocity").value);
   let simSpeed = Number(document.getElementById("simSpeed").value);
+  let graphUpdateInterval = Number(document.getElementById("graphint").value);
+  let graphLen = Number(document.getElementById("graphlen").value);
+  const trailLen = Number(document.getElementById("trailLen").value);
+  let project = document.getElementById("projection").checked;
 
 
 
   const geometryHoop = new THREE.TorusGeometry(100,tube,radialSegments,tubularSegments, arc);
   const materialHoop = new THREE.MeshBasicMaterial({color: 0x44aa88}); 
   const geometryHoop2 = new THREE.TorusGeometry(100,tube*1.05,radialSegments,tubularSegments, arc/2);
-  //const materialHoop2 = new THREE.MeshBasicMaterial({color: 0x0000FF}); 
   const hoop = new THREE.Mesh(geometryHoop, materialHoop);
-  //const hoop2 = new THREE.Mesh(geometryHoop2, materialHoop2);
-  //hoop2.rotation.z = 6.28/4;
   scene.add(hoop);
-  //scene.add(hoop2);
   const geometryCenter = new THREE.SphereGeometry( 2, 32, 16 );
   const materialCenter = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
   const center = new THREE.Mesh( geometryCenter, materialCenter );
@@ -70,7 +75,7 @@ export function draw(equations, useEval, thetaDivId, velocityDivId, ) {
 
 
 
-  const trailLen = Number(document.getElementById("trailLen").value);
+  //sets up for the ball trail
   let balls = [];
   let ballsCords = [];
   for (let i = 0; i < trailLen; i++) {
@@ -79,15 +84,13 @@ export function draw(equations, useEval, thetaDivId, velocityDivId, ) {
   }
   balls.forEach(e=>scene.add(e));
   let prevCords = []; 
-  prevCords.length = trailLen; prevCords.fill(0);;
-  //let funcID = Math.random();
+  prevCords.length = trailLen; prevCords.fill(0);
 
-  let shouldGraph = true;
-  let graphTimer = 0;
-  let graphUpdateInterval = Number(document.getElementById("graphint").value);
-  let graphLen = Number(document.getElementById("graphlen").value);
-  let project = document.getElementById("projection").checked;
-  let graphData = getGraphData(graphUpdateInterval, velocity, angle, omega, radius, g, k, equations, useEval, graphLen, window.evaluatex);
+  //let funcID = Math.random(); // use this if need to check to see which function is doing what or if the function isnt correctly being canceled
+  
+
+  //gets data for graph for d3 graphs and then graphs them for the correct simulation
+  let graphData = getGraphData(graphUpdateInterval, velocity, angle, omega, radius, g, k, equations, useEval, graphLen);
   if (thetaDivId === "variableSim-theta"){
   drawTheta(graphData, graphLen, thetaDivId, "inputed");
   drawVelocity(graphData, graphLen, velocityDivId, "inputed");
@@ -100,6 +103,7 @@ export function draw(equations, useEval, thetaDivId, velocityDivId, ) {
   let timer = 0;
   let lastTime = 0;
   let firstIteration = true;
+
   renderer.render(scene, camera);
   function render(time){ // find delta t between animations and plug in as h in rk4
     //console.log(funcID);
@@ -107,23 +111,26 @@ export function draw(equations, useEval, thetaDivId, velocityDivId, ) {
       lastTime = time;
       firstIteration = false;
     }
-    graphTimer += (time - lastTime)/1000;
-    let dt = ((time - lastTime) *simSpeed/ 1000);//simspeed kinda causes unexpected behavior
+
+    //hoop rotation, grab new data from rk4, adjust angle to whithin 0-6.28 rads
+    let dt = ((time - lastTime) *simSpeed/ 1000);
     timer += dt;
     lastTime = time;
     let data = updateVals(dt, velocity, angle, omega, radius, g, k, equations, useEval);
     angle = data[0];
-    if (project){
-      ballsCords.push(angle);
-      ballsCords.shift();
-    }
     angle = angle%(2*Math.PI);
     if (angle < 0){
       angle = 2*Math.PI - Math.abs(angle)
     }
     velocity = data[1];
     hoop.rotation.y += omega*dt;
-    //hoop2.rotation.y = hoop.rotation.y;
+    
+
+    //takes care of ball trail, and whether or not its projected on the hoop
+    if (project){
+      ballsCords.push(angle);
+      ballsCords.shift();
+    }
     let cords = getBallPos(angle+3*Math.PI/2, 100);
     let xyz = {x:cords[0]*Math.cos(hoop.rotation.y), y: cords[1], z: -cords[0]*Math.sin(hoop.rotation.y)};
     ball.position.set(xyz.x,xyz.y,xyz.z);
@@ -140,10 +147,12 @@ export function draw(equations, useEval, thetaDivId, velocityDivId, ) {
     }
     }
     
+    //update time on page
     document.getElementById("time").innerHTML = timer.toFixed(3);
     renderer.render(scene,camera);
 
-    if (useEval){
+
+    if (useEval){ //theres keep track of the animation frameloop for each simulation so it can be cancelled incase of regeneration
         nextFrameVariable = requestAnimationFrame(render);
     } else{
         nextFrameStatic = requestAnimationFrame(render);
