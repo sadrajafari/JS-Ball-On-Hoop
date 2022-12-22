9b8cf251cc8cfb0e093277839762e79428255508
A02366638 <a02366638@aggies.usu.edu>
2022-12-22 14:25:46 -0500

    default omega a little bigger, also trailLen

--- src/makeTable.js
+++ src/makeTable.js
@@ -4,13 +4,13 @@ vars = {r:{val: ".1", id: "radius", name: "r", type: "text"},
   k:{val: "5.0", id: "friction", name: "k", type: "text"},
    theta:{val: "5.0", id: "theta", name: "θ", type: "text"},
     v:{val: "0.6", id: "velocity", name: "v", type: "text"},
-     omega:{val: "11", id: "omega", name: "ω", type: "text"},
+     omega:{val: "20", id: "omega", name: "ω", type: "text"},
       simSpeed:{val: "0.3", id: "simSpeed", name: "simulation speed", type: "text"},
        graphUpdateInterval:{val: ".005", id: "graphint", name: "graph update interval", type: "text"},
        graphLen:{val: "5", id: "graphlen", name: "graph record length (s)", type: "text"},
        project:{val: "true", id: "projection", name: "Project trail on hoop?", type: "checkbox"},
        graphWrap:{val: "true", id: "wrap", name: "graph theta beyond 0-6.28", type: "checkbox"},
-       trailLen:{val: "20", id: "trailLen", name: "trail length", type: "text"}
+       trailLen:{val: "100", id: "trailLen", name: "trail length", type: "text"}
 
     };
 
